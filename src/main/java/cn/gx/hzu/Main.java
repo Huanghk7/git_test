082caa1e777e0cb1a20467c544cd2885afccf49e
Huanghk <512038254@qq.com>
2022-02-13 14:03:41 +0800

    first commit

---
+++ src/main/java/cn/gx/hzu/Main.java
@@ -0,0 +1,11 @@
+package cn.gx.hzu;
+
+/**
+ * @author hhkstart
+ * @create 2022-02-13 13:58
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
